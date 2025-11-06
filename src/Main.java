b2bb4d0bddadc3a61b33e4141816e5e94166cb81
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2025-11-06 09:51:38 +0800

    update: fixed errors and established testing for main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,61 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+
+        Rooster.ManokNaPuti player = new Rooster.ManokNaPuti("Red Rooster");
+        Rooster.ManokNaPuti enemy = new Rooster.ManokNaPuti("Blue Rooster");
+
+        System.out.println("=== ROOSTER BATTLE ===");
+        System.out.println(player.getName() + " vs " + enemy.getName());
+        System.out.println();
+
+
+        while (!player.isFainted() && !enemy.isFainted()) {
+
+            System.out.println("\n--- " + player.getName() + "'s Turn ---");
+            System.out.println("HP: " + player.getHp());
+            System.out.println("Choose your skill:");
+
+            List<Skill> playerSkills = player.getSkills();
+            for (int i = 0; i < playerSkills.size(); i++) {
+                Skill skill = playerSkills.get(i);
+                System.out.println((i + 1) + ". " + skill.getName() +
+                        " (Damage: " + skill.getDamage() +
+                        ", Type: " + skill.getType() + ")");
+            }
+
+            int choice = scanner.nextInt() - 1;
+            if (choice >= 0 && choice < playerSkills.size()) {
+                player.attack(enemy, playerSkills.get(choice));
+            } else {
+                System.out.println("Invalid choice! Using basic attack.");
+                player.attack(enemy);
+            }
+
+            if (enemy.isFainted()) {
+                System.out.println("\n" + enemy.getName() + " has fainted!");
+                System.out.println(player.getName() + " wins!");
+                break;
+            }
+
+
+            System.out.println("\n--- " + enemy.getName() + "'s Turn ---");
+            System.out.println("HP: " + enemy.getHp());
+
+            List<Skill> enemySkills = enemy.getSkills();
+            int randomSkill = (int) (Math.random() * enemySkills.size());
+            enemy.attack(player, enemySkills.get(randomSkill));
+
+            if (player.isFainted()) {
+                System.out.println("\n" + player.getName() + " has fainted!");
+                System.out.println(enemy.getName() + " wins!");
+                break;
+            }
+        }
+
+        scanner.close();
+        System.out.println("\n=== BATTLE END ===");
     }
 }
